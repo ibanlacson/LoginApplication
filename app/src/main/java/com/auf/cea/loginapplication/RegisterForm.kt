bc8cd9aabdf80399c82e32154a5219a0a835ee68
Ivan Lacson <lacson.ivan@auf.edu.ph>
2022-09-09 18:54:22 +0800

    Initial commit

---
+++ app/src/main/java/com/auf/cea/loginapplication/RegisterForm.kt
@@ -0,0 +1,11 @@
+package com.auf.cea.loginapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterForm : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register_form)
+    }
+}
