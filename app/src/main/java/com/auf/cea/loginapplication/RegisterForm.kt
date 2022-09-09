3d453ca4610f9b8a540cc72b9a38af6342029e07
Ivan Lacson <lacson.ivan@auf.edu.ph>
2022-09-09 19:15:00 +0800

    RegisterForm.kt started coding

--- app/src/main/java/com/auf/cea/loginapplication/RegisterForm.kt
+++ app/src/main/java/com/auf/cea/loginapplication/RegisterForm.kt
@@ -2,10 +2,17 @@ package com.auf.cea.loginapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.auf.cea.loginapplication.databinding.ActivityRegisterFormBinding
 
 class RegisterForm : AppCompatActivity() {
+    private lateinit var binding: ActivityRegisterFormBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register_form)
+        binding = ActivityRegisterFormBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
     }
 }
