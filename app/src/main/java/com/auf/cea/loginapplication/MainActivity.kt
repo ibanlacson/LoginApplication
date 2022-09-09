b8c314b2691897dd63377efe60d57c6e21c499c7
Ivan Lacson <lacson.ivan@auf.edu.ph>
2022-09-09 19:07:03 +0800

    SplashScreen.kt done

--- app/src/main/java/com/auf/cea/loginapplication/MainActivity.kt
+++ app/src/main/java/com/auf/cea/loginapplication/MainActivity.kt
@@ -3,10 +3,11 @@ package com.auf.cea.loginapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
 
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
